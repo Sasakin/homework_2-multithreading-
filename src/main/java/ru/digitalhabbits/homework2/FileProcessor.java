c808bdabfee1e9519ddd00ee83a9023abac0dc2f
Sasakin Maxim <max_to_day@Mail.ru>
2022-03-19 23:11:36 +0300

    Update cucncurrent logic. Add synchronization using semaphore.

--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -5,13 +5,11 @@ import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Stream;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 import static java.lang.Runtime.getRuntime;
 import static java.nio.charset.Charset.defaultCharset;
@@ -21,49 +19,66 @@ public class FileProcessor {
     private static final Logger logger = getLogger(FileProcessor.class);
     public static final int CHUNK_SIZE = 2 * getRuntime().availableProcessors();
 
+    Semaphore semaphore = new Semaphore(-CHUNK_SIZE + 1);
+
     public void process(@Nonnull String processingFileName, @Nonnull String resultFileName) {
+        long millisec = System.currentTimeMillis();
+
         checkFileExists(processingFileName);
 
         final File file = new File(processingFileName);
-        FileWriter fileWriter = new FileWriter(resultFileName);
+        FileWriter fileWriter = new FileWriter(resultFileName, semaphore);
         Thread writerThread = new Thread(fileWriter);
-        writerThread.start();
 
         ExecutorService executor = Executors.newFixedThreadPool(CHUNK_SIZE);
+        List<Callable<Pair<String, Integer>>> threadList = new ArrayList<>();
+        List<Future<Pair<String, Integer>>> futureList = new ArrayList<>();
         try (final Scanner scanner = new Scanner(file, defaultCharset())) {
             while (scanner.hasNext()) {
 
-                Stream<Callable<Pair<String, Integer>>> threadStream = Stream.generate(() -> {
-                    if (scanner.hasNextLine()) {
-                        String line = scanner.nextLine();
-                        Callable<Pair<String, Integer>> lineThread = new LineProcessorThread(line);
-                        return lineThread;
+                while (threadList.size() < CHUNK_SIZE && scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    Callable<Pair<String, Integer>> lineThread = new LineProcessorThread(line, semaphore);
+                    threadList.add(lineThread);
+                }
+
+                threadList.forEach(t -> {
+                    try {
+                        futureList.add(executor.submit(t));
+                    } catch (Throwable e) {
+                        logger.error("", e);
                     }
-                    return null; // TODO fixmy
                 });
 
-                threadStream.limit(CHUNK_SIZE).filter(t -> t!=null).forEach(t -> {
+                semaphore.acquire(1);
+
+                fileWriter.setFuture(futureList.stream().map(f -> {
                     try {
-                        fileWriter.setFuture(executor.submit(t).get());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
+                        return f.get();
+                    } catch (Throwable e) {
                         e.printStackTrace();
                     }
-                });
+                    return null;
+                }).collect(Collectors.toList()));
+
+
+                executor.submit(writerThread);
+
+                semaphore.acquire(1);
+
+                threadList.clear();
+                futureList.clear();
 
             }
-        } catch (IOException exception) {
+        } catch (Throwable exception) {
             logger.error("", exception);
-            executor.shutdown();
-            fileWriter.shutdown();
         } finally {
             executor.shutdown();
-            fileWriter.shutdown();
             logger.info("Потоки закончили работу");
         }
 
         logger.info("Finish main thread {}", Thread.currentThread().getName());
+        logger.info("Process time: {}", System.currentTimeMillis() - millisec);
     }
 
     private void checkFileExists(@Nonnull String fileName) {
--- src/main/java/ru/digitalhabbits/homework2/LineProcessorThread.java
+++ src/main/java/ru/digitalhabbits/homework2/LineProcessorThread.java
@@ -3,17 +3,23 @@ package ru.digitalhabbits.homework2;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.Semaphore;
 
 public class LineProcessorThread implements Callable<Pair<String, Integer>> {
 
     private final String line;
 
-    public LineProcessorThread(String line) {
+    private final Semaphore semaphore;
+
+    public LineProcessorThread(String line, Semaphore semaphore) {
         this.line = line;
+        this.semaphore = semaphore;
     }
 
     @Override
     public Pair<String, Integer> call() throws Exception {
-        return new LineCounterProcessor().process(line);
+        Pair pair = new LineCounterProcessor().process(line);
+        semaphore.release(1);
+        return  pair;
     }
 }
